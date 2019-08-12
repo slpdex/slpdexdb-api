781746c115e76e901e16bb7d9bf725633f7b2b73
Tobias <ruck.tobias@gmail.com>
2019-08-12 11:54:22 +0200

    Added types.ts

---
+++ types.ts
@@ -0,0 +1,51 @@
+export interface AddressUtxo {
+  type: 'AddressUtxo',
+  addUtxos: AddAddressUtxo[],
+  removeUtxos: RemoveUtxo[],
+}
+
+export interface TxHistory {
+  type: "TxHistory",
+  addTxHistory: TxDelta[],
+}
+
+export interface TradeOfferUtxo {
+  type: "TradeOfferUtxo",
+  addUtxos: AddTradeOfferUtxo[],
+  removeUtxos: RemoveUtxo[],
+}
+
+interface AddAddressUtxo {
+  tx: string,
+  vout: number,
+  valueSatoshis: number,
+  valueToken: string,
+  valueTokenBase: string,
+  tokenIdHex: string,
+}
+
+interface AddTradeOfferUtxo {
+  outputVout: number,
+  inputTx: string,
+  inputVout: number,
+  pricePerToken: string,
+  scriptPrice: string,
+  isInverted: boolean,
+  sellAmountTokenBase: string,
+  receivingAddress: string,
+  tokenIdHex: string,
+}
+
+interface RemoveUtxo {
+  tx: string,
+  vout: number,
+}
+
+interface TxDelta {
+  tx: string,
+  deltaSatoshis: number,
+  deltaToken: string,
+  deltaTokenBase: string,
+  tokenIdHex: string,
+  timestamp: number,
+}
