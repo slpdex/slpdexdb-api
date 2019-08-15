00ae9c464a95e97bcbfebf9d89e3b2759f7e5e0c
tobiasruck <ruck.tobias@gmail.com>
2019-08-15 21:38:15 +0200

    Fixed type of

--- types.ts
+++ types.ts
@@ -1,51 +1,51 @@
 export interface AddressUtxo {
-  type: 'AddressUtxo',
-  addUtxos: AddAddressUtxo[],
-  removeUtxos: RemoveUtxo[],
+  type: 'AddressUtxo'
+  addUtxos: AddAddressUtxo[]
+  removeUtxos: RemoveUtxo[]
 }
 
 export interface TxHistory {
-  type: "TxHistory",
-  addTxHistory: TxDelta[],
+  type: "TxHistory"
+  addTxHistory: TxDelta[]
 }
 
 export interface TradeOfferUtxo {
-  type: "TradeOfferUtxo",
-  addUtxos: AddTradeOfferUtxo[],
-  removeUtxos: RemoveUtxo[],
+  type: "TradeOfferUtxo"
+  addUtxos: AddTradeOfferUtxo[]
+  removeUtxos: RemoveUtxo[]
 }
 
 interface AddAddressUtxo {
-  tx: string,
-  vout: number,
-  valueSatoshis: number,
-  valueToken: string,
-  valueTokenBase: string,
-  tokenIdHex: string,
+  tx: string
+  vout: number
+  valueSatoshis: number
+  valueToken: string
+  valueTokenBase: string
+  tokenIdHex: string | null
 }
 
 interface AddTradeOfferUtxo {
-  outputVout: number,
-  inputTx: string,
-  inputVout: number,
-  pricePerToken: string,
-  scriptPrice: string,
-  isInverted: boolean,
-  sellAmountTokenBase: string,
-  receivingAddress: string,
-  tokenIdHex: string,
+  outputVout: number
+  inputTx: string
+  inputVout: number
+  pricePerToken: string
+  scriptPrice: string
+  isInverted: boolean
+  sellAmountTokenBase: string
+  receivingAddress: string
+  tokenIdHex: string
 }
 
 interface RemoveUtxo {
-  tx: string,
-  vout: number,
+  tx: string
+  vout: number
 }
 
 interface TxDelta {
-  tx: string,
-  deltaSatoshis: number,
-  deltaToken: string,
-  deltaTokenBase: string,
-  tokenIdHex: string,
-  timestamp: number,
+  tx: string
+  deltaSatoshis: number
+  deltaToken: string
+  deltaTokenBase: string
+  tokenIdHex: string | null
+  timestamp: number
 }
